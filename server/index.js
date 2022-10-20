2fd0ca1e5d220046175231807f018942800b8a8a
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-10-21 01:38:56 +0200

    connected to db

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const port = process.env.PORT || 8000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(port, () => console.log(`Server running on port ${port}`))
+  )
+  .catch((err) => console.log(err.message));
+
+  
+
+
