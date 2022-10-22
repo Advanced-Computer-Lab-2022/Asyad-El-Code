304d169c4d0fe4dc04257e7d6df8c1b54a1ec28b
ahmedkhedr01 <ahmedkhedr2910@gmail.com>
2022-10-22 21:02:58 +0200

    modified instructor schema and view course titles method

--- server/controller/instructor.js
+++ server/controller/instructor.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import Instructor from "../models/instructor.js";
+import Course from "../models/course.js";
 import { validate } from "../models/instructor.js";
 
 export const createInstructor = async (req, res) => {
@@ -35,3 +36,25 @@ export const createInstructor = async (req, res) => {
     res.send(error.message); //test
   }
 };
+
+export const viewCourseTitles = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const courses = await Course.find();
+  const courseTitles = [];
+  for (let i = 0; i < courses.length; i++) {
+    let instructorsId = courses[i].instructors;
+    for (let j = 0; j < instructorsId.length; j++) {
+      console.log(instructorsId[j]);
+
+      courseTitles.push(
+        instructorsId[j].toString() == id ? courses[i].title : null
+      );
+    }
+  }
+  res.send(courseTitles);
+};
+
+// export const filterCourses = async (req, res) => {
+
+// };
