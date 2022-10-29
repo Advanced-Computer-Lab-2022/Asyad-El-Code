b6f38ed9ad4e508c4e69c3fe54c9fa9e1fd72dc0
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-10-29 18:36:33 +0200

    Popular courses  component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
 import React from "react";
-import {
-  AppBar,
-  CssBaseline,
-  Grid,
-  styled,
-  Tabs,
-  TextField,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+
 import { ThemeContext } from "@emotion/react";
 import { Stack } from "@mui/system";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import Navbar from "./components/Navbar";
 import MenuAppBar from "./components/Navbar";
 import Home from "./components/HomePage/Home";
+import CourseDetails from "./components/Course/CourseDetails";
 export const App = () => {
   return (
     <>
       <MenuAppBar></MenuAppBar>
       <Home></Home>
+
+      {/* {/* <CourseDetails></CourseDetails> */}
     </>
   );
 };
