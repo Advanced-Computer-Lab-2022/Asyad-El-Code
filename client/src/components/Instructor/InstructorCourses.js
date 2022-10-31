eb2523996d0174e21109298ba8fa2abd26fddc06
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-10-31 20:00:15 +0200

    merge

--- client/src/components/Instructor/InstructorCourses.js
+++ client/src/components/Instructor/InstructorCourses.js
@@ -2,11 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import {
   getAllInstructorCourses,
-<<<<<<< HEAD
   filterInstructorCourses,
-=======
-  filterInstructorCourses
->>>>>>> 9816e0d6c27ff5ecd354470d9965c693bad548d6
 } from "../../actions/instructor";
 import { FilterBar } from "../ViewAllCoursesPage/FilterBar";
 import { CoursesGrid } from "../ViewAllCoursesPage/CoursesGrid";
