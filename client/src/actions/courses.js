d9564fb5564135fc0c74cf463f04d03b1d0dcf54
ahmedkhedr01 <ahmedkhedr2910@gmail.com>
2022-10-31 02:21:11 +0200

    created instructor api and actions

--- client/src/actions/courses.js
+++ client/src/actions/courses.js
@@ -11,25 +11,12 @@ export const getCourses = () => async (dsipatch) => {
 };
 export const filterCourses = (filterData) => async (dispatch) => {
   try {
-    const { data } = await api.filterCourses(filterData);
+    const { data } = await courseApi.filterCourses(filterData);
     dispatch({ type: "FILTER_SUBJECT_RATING", payload: data });
   } catch (error) {
     console.log(error);
   }
 };
-export const getAllInstructorCourses = () => async (dispatch) => {
-  try {
-    const { data } = await api.getAllInstructorCourses();
-    dispatch({ type: "FETCH_ALL_INSTRUCTOR_COURSES", payload: data });
-  } catch (error) {
-    console.log(error);
-  }
-};
-export const filterInstructorCourses = (filterData) => async (dispatch) => {
-  try {
-    const { data } = await api.filterInstructorCourses(filterData);
-    dispatch({ type: "FILTER_INSTRUCTOR_COURSES", payload: data });
-
 
 export const filterByTilteOrSubjectOrInstructor =
   (searchQuery) => async (dispatch) => {
@@ -48,7 +35,6 @@ export const createCourse = (course) => async (dispatch) => {
   try {
     const { data } = await courseApi.createCourse(course);
     dispatch({ type: CREATE_COURSE, payload: data });
-
   } catch (error) {
     console.log(error);
   }
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,20 +1,3 @@
 import axios from "axios";
 
 const API = axios.create({ baseURL: "http://localhost:8000" });
-
-
-export const fetchCourses = async () => await API.get(`/course/getCourses`);
-export const filterCourses = async (filterData) =>
-  await API.get(
-    `/course/filterAllCourses/?subject=${filterData.Subject}&price=${filterData.Price}&rating=${filterData.Rating}`
-  );
-export const getAllInstructorCourses = async () =>
-  await API.get(`/instructor/getAllInstructorCourses/635c587e07f18b986c357bb7`);
-export const filterInstructorCourses = async (filterData) => {
-  return await API.get(
-    `/instructor/filterInstructorCourses/?id=635c587e07f18b986c357bb7&subject=${filterData.Subject}&price=${filterData.Price}&rating=${filterData.Rating}`
-  );
-};
-
-
-
