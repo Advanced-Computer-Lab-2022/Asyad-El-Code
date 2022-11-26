775b4c262a6ffbf89a2b92d0ed658ef49f6e5c9d
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-11-26 22:47:08 +0200

    styling homepage

--- client/src/App.js
+++ client/src/App.js
@@ -8,31 +8,37 @@ import { Switch, Route } from "react-router-dom";
 import CourseStructure from "./components/Instructor/CourseStructure";
 import Admin from "./components/Admin/Admin.js";
 import Exercise from "./components/Instructor/Exercise";
+import CoursePage from "./components/Course/CoursePage";
+import ScrollToTop from "./components/ScrollToTop";
 export const App = () => {
   return (
     <>
       {/* <MenuAppBar></MenuAppBar>
       <Home></Home> */}
       <Navbar></Navbar>
+      <ScrollToTop>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/viewAll">
+            <ViewAllCourses />
+          </Route>
+          <Route exact path="/instructorpage">
+            <InstructorCourses></InstructorCourses>
+          </Route>
+          <Route exact path="/createCourse">
+            <CourseStructure></CourseStructure>
+          </Route>
+          <Route path="/adminPage">
+            <Admin></Admin>
+          </Route>
 
-
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/viewAll">
-          <ViewAllCourses />
-        </Route>
-        <Route exact path="/instructorpage">
-          <InstructorCourses></InstructorCourses>
-        </Route>
-        <Route exact path="/createCourse">
-          <CourseStructure></CourseStructure>
-        </Route>
-        <Route path="/adminPage">
-          <Admin></Admin>
-        </Route>
-      </Switch>
+          <Route path="/course/*">
+            <CoursePage></CoursePage>
+          </Route>
+        </Switch>
+      </ScrollToTop>
 
       {/* <CourseDetails></CourseDetails> */}
 
