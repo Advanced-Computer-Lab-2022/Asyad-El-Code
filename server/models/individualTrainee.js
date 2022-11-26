6c6b719214e7658725e5df52d65f0e81ba750303
ahmedheidar <ahmedsabokhater@gmail.com>
2022-11-26 18:43:24 +0200

    add courses to individualTrainee Modal

--- server/models/individualTrainee.js
+++ server/models/individualTrainee.js
@@ -3,48 +3,48 @@ import mongoose from "mongoose";
 import Joi from "joi";
 
 
-const individualTraineeSchema = mongoose.Schema ({
-    firstName:{
+const individualTraineeSchema = mongoose.Schema({
+    firstName: {
         type: String,
         required: true,
         minLength: 3
     },
 
-    lastName:{
+    lastName: {
         type: String,
         required: true,
         minLength: 3
     },
 
-    password:{
+    password: {
         type: String,
         required: true
     },
 
-    email:{
+    email: {
         type: String,
         required: true,
         unique: true,
     },
 
-    country:{
+    country: {
         type: String,
         required: true
     },
 
-    billingDetails:{
-        masterCardNumber:String,
+    billingDetails: {
+        masterCardNumber: String,
         expiryDate: Date,
         cvv: String,
         cardOwner: String,
-       
+
     },
 
     // certificate:{
     //     type: [String]
     // },
 
-    
+
     // problems:{
     //     type:[String]  //problem related to specific course 
     // },
@@ -58,55 +58,89 @@ const individualTraineeSchema = mongoose.Schema ({
     //     default:0
     // },
 
-    dateOfBirth:{
-        type:Date,
+    dateOfBirth: {
+        type: Date,
         required: true
     },
 
-    phoneNumber:{
-        type:String,
-        required:true
-    },
-
-    university:{
-        type:String,
+    phoneNumber: {
+        type: String,
         required: true
-        
     },
 
-    address:{
+    university: {
+        type: String,
+        required: true
+
+    },
+
+    address: {
         city: String,
         streetName: String,
         streetNumber: String
     },
-    
 
-    // COURSES
+
+    courses: [{
+        courseId: { type: mongoose.Schema.Types.ObjectId, ref: "Course" },
+        title: {
+            type: String,
+            required: true,
+        },
+        summary: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        releaseDate: {
+            type: Date,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        rating: {
+            type: Number,
+            default: 0.0,
+        },
+        instructor: {
+
+            instructorId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Instructor",
+            },
+            name: String,
+        },
+    }]
 });
 
 
 export function validate(individualTrainee) {
     const schema = Joi.object({
-      firstName: Joi.string().min(3).required(),
-      lastName: Joi.string().min(3).required(),
-      email: Joi.string().email().required(),
-      phoneNumber: Joi.number().required().min(10),
-      dateOfBirth: Joi.date().required(),
-      password: Joi.string().required(),
-      address: Joi.object().required(),
-      country: Joi.string().required(),
-      university: Joi.string().required(),
-      //problems: Joi.array().required(),
-      //certificate: Joi.array().required(),
-      billingDetails: Joi.object({
-        masterCardNumber: Joi.string().required(),
-        expiryDate: Joi.date().required(),
-        cvv: Joi.string().required(),
-        cardOwner: Joi.string().required(),
-      })
+        firstName: Joi.string().min(3).required(),
+        lastName: Joi.string().min(3).required(),
+        email: Joi.string().email().required(),
+        phoneNumber: Joi.number().required().min(10),
+        dateOfBirth: Joi.date().required(),
+        password: Joi.string().required(),
+        address: Joi.object().required(),
+        country: Joi.string().required(),
+        university: Joi.string().required(),
+        //problems: Joi.array().required(),
+        //certificate: Joi.array().required(),
+        billingDetails: Joi.object({
+            masterCardNumber: Joi.string().required(),
+            expiryDate: Joi.date().required(),
+            cvv: Joi.string().required(),
+            cardOwner: Joi.string().required(),
+        })
     });
     return schema.validate(individualTrainee);
-  }
-  
-  const IndividualTrainee = mongoose.model("IndividualTrainee", individualTraineeSchema);
-  export default IndividualTrainee;
+}
+
+const IndividualTrainee = mongoose.model("IndividualTrainee", individualTraineeSchema);
+export default IndividualTrainee;
