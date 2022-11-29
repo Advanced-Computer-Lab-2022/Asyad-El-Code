03a52b2f6006adafc35ef0741012a9091f207ff8
ahmedkhedr01 <ahmedkhedr2910@gmail.com>
2022-11-29 02:08:18 +0200

    added styling for course content page

---
+++ client/src/css/courseContent.js
@@ -0,0 +1,38 @@
+import { makeStyles } from "tss-react/mui";
+
+const useStyles = makeStyles()((theme) => {
+  return {
+    root: {
+      marginTop: "18px",
+    },
+    paper: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      padding: "20px",
+      backgroundColor: "#1C1D1F",
+      marginBottom: "20px",
+    },
+    submit: {
+      margin: "20px 0px 10px",
+    },
+    courseTitle: {
+      color: "white",
+      fontWeight: "bold",
+      fontSize: 20,
+    },
+    exercisePaper: {
+      padding: "20px",
+      backgroundColor: "#EBF1F2",
+      marginBottom: "20px",
+    },
+    homeIcon: {
+      color: "primary",
+      "&:hover": {
+        color: "secondary",
+      },
+    },
+  };
+});
+export default useStyles;
