60cc18e3a510ee22e68d2e85ec037a5f9d4a79a2
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-12-04 17:44:31 +0200

    merge conflicts

--- client/src/components/Profile/Payments.js
+++ client/src/components/Profile/Payments.js
@@ -1,60 +1,82 @@
 import {
-    Grid, Container, Typography, Paper,
-    Modal, FormControl, form, OutlinedInput, Input, InputLabel,
-    FormGroup, Button, IconButton, TextField, MenuItem, Select, InputAdornment
-} from '@mui/material'
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
+  Grid,
+  Container,
+  Typography,
+  Paper,
+  Modal,
+  FormControl,
+  form,
+  OutlinedInput,
+  Input,
+  InputLabel,
+  FormGroup,
+  Button,
+  IconButton,
+  TextField,
+  MenuItem,
+  Select,
+  InputAdornment,
+} from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { getTrainee } from '../../actions/individualTrainees';
-import { updateTrainee } from '../../actions/individualTrainees';
-
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { getTrainee } from "../../actions/individualTrainees";
+import { updateTrainee } from "../../actions/individualTrainees";
 
 const Payments = () => {
-    const trainee = useSelector((t) => t.individualTrainee)
-    const dispatch = useDispatch();
-    const [firstName, setFirstName] = useState(trainee.firstName);
-    const [lastName, setLastName] = useState(trainee.lastName);
-    const [password, setPassword] = useState(trainee.password);
-    const [phone, setPhone] = useState(trainee.phoneNumber);
-    const [country, setCountry] = useState(trainee.country);
-    const [date, setDate] = useState(trainee.dateOfBirth);
-    const [showPassword, setShowPassword] = useState(false);
-    const handleClickShowPassword = () => {
-        setShowPassword(!showPassword)
-    };
+  const trainee = useSelector((t) => t.individualTrainee);
+  const dispatch = useDispatch();
+  const [firstName, setFirstName] = useState(trainee.firstName);
+  const [lastName, setLastName] = useState(trainee.lastName);
+  const [password, setPassword] = useState(trainee.password);
+  const [phone, setPhone] = useState(trainee.phoneNumber);
+  const [country, setCountry] = useState(trainee.country);
+  const [date, setDate] = useState(trainee.dateOfBirth);
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-    };
-    console.log(trainee)
-    const handleForm = (e) => {
-        e.preventDefault()
-        trainee.firstName = firstName
-        trainee.lastName = lastName
-        trainee.password = password
-        trainee.phoneNumber = phone
-        trainee.country = country
-        dispatch(updateTrainee(trainee._id, trainee))
-        console.log(trainee)
-    }
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  console.log(trainee);
+  const handleForm = (e) => {
+    e.preventDefault();
+    trainee.firstName = firstName;
+    trainee.lastName = lastName;
+    trainee.password = password;
+    trainee.phoneNumber = phone;
+    trainee.country = country;
+    dispatch(updateTrainee(trainee._id, trainee));
+    console.log(trainee);
+  };
 
-    useEffect(() => {
-        dispatch(getTrainee())
-    }, [])
+  useEffect(() => {
+    dispatch(getTrainee());
+  }, []);
 
-    return (
-        <Grid container direction="column" justifyContent="center" alignContent="center" color="#1C1D1F">
-            <Grid item borderBottom={1} borderColor="silver" width="100%" padding={3}>
-                <Typography fontWeight="bold" fontSize={35} textAlign="center">Payments</Typography>
-                <Typography fontSize={20} textAlign="center">Add your payment information</Typography>
-            </Grid>
-            {/* <Grid item borderBottom={1} borderColor="silver" width="100%" padding={3}>
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignContent="center"
+      color="#1C1D1F"
+    >
+      <Grid item borderBottom={1} borderColor="silver" width="100%" padding={3}>
+        <Typography fontWeight="bold" fontSize={35} textAlign="center">
+          Payments
+        </Typography>
+        <Typography fontSize={20} textAlign="center">
+          Add your payment information
+        </Typography>
+      </Grid>
+      {/* <Grid item borderBottom={1} borderColor="silver" width="100%" padding={3}>
                 <form onSubmit={handleForm}>
 
                     <FormGroup sx={{ marginTop: '20px' }}>
@@ -191,8 +213,8 @@ const Payments = () => {
                     </FormGroup>
                 </form>
             </Grid> */}
-        </Grid>
-    )
-}
+    </Grid>
+  );
+};
 
 export default Payments;
