8724ba2004aa623fe4e1c81b7fa2fd60484b6a8b
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-12-10 05:06:28 +0200

    fix bugs in mycourses

--- client/src/components/Trainee/MyCourses.js
+++ client/src/components/Trainee/MyCourses.js
@@ -62,56 +62,56 @@ const MyCourses = () => {
           <h1>Helloworld</h1>
         </Grid>
       </Grid>
-      {courses.map((course, index) => {
-        return (
-          <Grid item xs={9}>
-            <Card sx={{ overFlow: "hidden", display: "flex" }}>
-              <CardMedia
-                sx={{
-                  maxWidth: 500,
-                  maxHeight: 300,
-                  maxInlineSize: 500,
-                  display: "inline-block",
-                }}
-                component="img"
-                image={image}
-              />
-              <CardContent sx={{ display: "inline-block", maxWidth: 500 }}>
-                <Typography variant="h5" component="div">
-                  {course.title}
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} variant="body2">
-                  {course.summary}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Updated{" "}
-                  <b>
-                    {new Intl.DateTimeFormat("en-US", {
-                      year: "numeric",
-                      month: "short",
-                      day: "2-digit",
-                    }).format(new Date(Date.parse(course.releaseDate)))}{" "}
-                  </b>
-                  {course.duration} total hours {bull} 52 lectures
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  By <b>SADSD</b>
-                </Typography>
-                <Stack spacing={1} direction="row">
-                  <p>4.5</p>
-                  <Rating readOnly sx={{ alignItems: "center" }}></Rating>
-                  <p style={{ alignSelf: "center" }}></p>
-                </Stack>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Go To Course</Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        );
-      })}
-    </Grid>
     </Container>
+
+    //   {courses.map((course, index) => {
+    //     return (
+    //       <Grid item xs={9}>
+    //         <Card sx={{ overFlow: "hidden", display: "flex" }}>
+    //           <CardMedia
+    //             sx={{
+    //               maxWidth: 500,
+    //               maxHeight: 300,
+    //               maxInlineSize: 500,
+    //               display: "inline-block",
+    //             }}
+    //             component="img"
+    //             image={image}
+    //           />
+    //           <CardContent sx={{ display: "inline-block", maxWidth: 500 }}>
+    //             <Typography variant="h5" component="div">
+    //               {course.title}
+    //             </Typography>
+    //             <Typography sx={{ mb: 1.5 }} variant="body2">
+    //               {course.summary}
+    //             </Typography>
+    //             <Typography variant="body2" color="text.secondary">
+    //               Updated{" "}
+    //               <b>
+    //                 {new Intl.DateTimeFormat("en-US", {
+    //                   year: "numeric",
+    //                   month: "short",
+    //                   day: "2-digit",
+    //                 }).format(new Date(Date.parse(course.releaseDate)))}{" "}
+    //               </b>
+    //               {course.duration} total hours {bull} 52 lectures
+    //             </Typography>
+    //             <Typography variant="body2" color="text.secondary">
+    //               By <b>SADSD</b>
+    //             </Typography>
+    //             <Stack spacing={1} direction="row">
+    //               <p>4.5</p>
+    //               <Rating readOnly sx={{ alignItems: "center" }}></Rating>
+    //               <p style={{ alignSelf: "center" }}></p>
+    //             </Stack>
+    //           </CardContent>
+    //           <CardActions>
+    //             <Button size="small">Go To Course</Button>
+    //           </CardActions>
+    //         </Card>
+    //       </Grid>
+    //     );
+    //   })}
     // <Grid container justifyContent="center" rowSpacing={4} marginBottom={10}>
     //   <Grid item xs={10} marginTop={5}>
     //     <Typography variant="h3">My courses</Typography>
