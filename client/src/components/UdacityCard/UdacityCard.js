c1f54d411f9f611589daf1507d92bf93ac1453d5
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-12-31 00:16:34 +0200

    adding udacityCard component

---
+++ client/src/components/UdacityCard/UdacityCard.js
@@ -0,0 +1,161 @@
+import {
+  Chip,
+  Divider,
+  Grid,
+  Paper,
+  Button,
+  Typography,
+  Card,
+  Stack,
+  Rating,
+} from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import image from "../../images/coding.jpeg";
+import { getRate } from "../util";
+
+export const UdacityCard = ({ course }) => {
+  const traineeType = JSON.parse(localStorage.getItem("profile"))?.type;
+  const selectedCountry = useSelector((c) => c.selectedCountry);
+  const rates = useSelector((c) => c.currencyRates);
+  console.log("UDACITY CARD" + " " + course);
+  console.log(course);
+
+  return (
+    <Card style={{ width: "600px", height: "430px" }}>
+      <Grid
+        columnSpacing={2}
+        padding={3}
+        height="100%"
+        width="100%"
+        container
+        direction="row"
+      >
+        <Grid
+          xs={4}
+          sx={{
+            //Set a border to the right only to know the limits of the grid
+            borderRight: "1px solid black",
+            borderColor: "#eeeeee",
+            height: "140%",
+            padding: "10px",
+          }}
+          rowSpacing={1}
+          item
+          container
+          direction="column"
+        >
+          <Grid item>
+            <img
+              style={{
+                height: "100%",
+                width: "100%",
+                objectFit: "cover",
+                //I want you to cut frrom the top right using ellipse and clippath
+                //   clipPath: "ellipse(150% 80% at 0% 90%)",
+                // clipPath: "circle(100% at 100% 90%)",
+              }}
+              src={image}
+            ></img>
+          </Grid>
+          <Grid item>
+            <Button
+              fullWidth
+              style={{
+                padding: "12px",
+                textTransform: "none",
+                backgroundColor: "#205295",
+              }}
+              variant="contained"
+            >
+              Program Details
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              fullWidth
+              style={{
+                padding: "12px",
+                borderColor: "#205295",
+                borderStyle: "solid",
+                borderWidth: "2px",
+                color: "#205295",
+                textTransform: "none",
+              }}
+              variant="outlined"
+            >
+              {traineeType === "instructor" || traineeType === "administrator"
+                ? "Define Promotion"
+                : "Buy Now"}
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid rowSpacing={2} item xs={8} container direction="column">
+          <Grid item>
+            <Chip
+              label={course.subject}
+              style={{
+                fontWeight: "bold",
+                backgroundColor: "#eeeeee",
+                color: "black",
+              }}
+              // color="success"
+            />
+          </Grid>
+          <Grid item>
+            <Typography variant="h5">{course.title}</Typography>
+          </Grid>
+          <Grid mt={-1} item>
+            <Typography fontSize={17} color="text.secondary" variant="body2">
+              {/* {course.price !== course.discountedPrice ? (
+                <span style={{ textDecoration: "line-through" }}>
+                  {getRate(selectedCountry, rates, course.price)}
+                </span>
+              ) : null}
+              {getRate(selectedCountry, rates, course.discountedPrice)} */}
+              {` ${course.discountedPrice} EGP`}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography fontSize={12} color="text.secondary" variant="body2">
+              Instructor: {course.instructor.name}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Stack
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+
+                backgroundColor: "#eeeeee",
+              }}
+              direction="row"
+              spacing={2}
+              // width="150%"
+              padding={2}
+            >
+              <Rating
+                name="read-only"
+                style={{ color: "black" }}
+                value={parseInt(course.rating)}
+                readOnly
+                size="small"
+              />
+              <Typography fontSize={12} variant="body2">
+                {course.rating} (1,000+ reviews)
+              </Typography>
+            </Stack>
+          </Grid>
+          <Grid item>
+            <Typography variant="h7">Course Summary</Typography>
+          </Grid>
+          <Grid item>
+            <Typography fontSize={12} color="text.secondary" variant="body2">
+              {course.summary}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Card>
+  );
+};
--- client/src/components/ViewAllCoursesPage/ViewAllCourses.js
+++ client/src/components/ViewAllCoursesPage/ViewAllCourses.js
@@ -1,24 +1,36 @@
 import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
-import { Button, Grid, MenuList, Typography } from "@mui/material";
+import { Button, Container, Grid, MenuList, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import Slider from "../Slider";
 import { CoursesGrid } from "./CoursesGrid";
 import { FilterBar } from "./FilterBar";
 import { useDispatch } from "react-redux";
 import { filterCourses, getCourses } from "../../actions/courses";
-
+import { useLocation } from "react-router-dom";
+import { getAllInstructorCourses } from "../../actions/instructor";
 export const Courses = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const search = queryParams.get("source");
+  console.log("search", search);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getCourses());
+    if (search === "instructor") {
+      dispatch(getAllInstructorCourses());
+    } else {
+      dispatch(getCourses());
+    }
   }, []);
+
   const handleClick = (e, filterData) => {
     dispatch(filterCourses(filterData));
   };
+
   return (
     <div>
       <FilterBar handleClick={handleClick}></FilterBar>
       <CoursesGrid></CoursesGrid>
+
       <Grid container justifyContent="center" marginTop="40px"></Grid>
     </div>
   );
